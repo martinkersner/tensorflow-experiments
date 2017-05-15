bugfix in computation of discount rewards

gym/cart-pole.py
@@ -9,21 +9,21 @@ import gym
 #action = 1
 #obs, reward, done, info = env.step(action)
 
-def dicount_rewards(rewards, discount_rate):
-  dicounted_rewards = np.empty(len(rewards))
+def discount_rewards(rewards, discount_rate):
+  discounted_rewards = np.empty(len(rewards))
   cumulative_rewards = 0
 
   for step in reversed(range(len(rewards))):
     cumulative_rewards = rewards[step] + cumulative_rewards * discount_rate
     discounted_rewards[step] = cumulative_rewards
 
-  return cumulative_rewards
+  return discounted_rewards
 
 def discount_and_normalize_rewards(all_rewards, discount_rate):
-  all_discounted_rewards = [discount_rewards(rewards) for rewards in all_rewards]
+  all_discounted_rewards = [discount_rewards(rewards, discount_rate) for rewards in all_rewards]
   flat_rewards = np.concatenate(all_discounted_rewards)
   reward_mean = flat_rewards.mean()
-  rewards_std = flat_rewards.std()
+  reward_std = flat_rewards.std()
 
   return [(discounted_rewards - reward_mean)/reward_std for discounted_rewards in all_discounted_rewards]
 